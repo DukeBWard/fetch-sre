7a147d811fb673ce5ad2b428ce816e70bf70f4f6
Luke Ward <wardlukeb@gmail.com>
2025-01-07 13:52:16 -0500

    health check

---
+++ README.md
@@ -0,0 +1,6 @@
+# prerequisites
+golang 1.22
+
+# third party libraries
+* `"gopkg.in/yaml.v2"`
+* `"github.com/robfig/cron"`
--- main.go
+++ main.go
@@ -3,12 +3,18 @@ package main
 import (
 	"flag"
 	"log"
+	"net/http"
+	"net/url"
 	"os"
+	"strings"
+	"time"
 
 	"github.com/robfig/cron"
 	"gopkg.in/yaml.v2"
 )
 
+// http endpoints
+// used map here, no dictionary in go
 type Endpoint struct {
 	Name    string            `yaml:"name"`
 	URL     string            `yaml:"url"`
@@ -40,6 +46,75 @@ func main() {
 		log.Fatalf("Failure to load YAML: ", err)
 	}
 
+	// block the main thread
+	select {}
+}
+
+func runChecks(endpoints []Endpoint, domainStatusMap map[string]*DomainStatus) {
+	client := http.Client{}
+
+	for _, endpoint := range endpoints {
+		// get hostname from url
+		parsedURL, err := url.Parse(endpoint.URL)
+		if err != nil {
+			log.Printf("invalid url: %s", endpoint.URL)
+			continue
+		}
+
+		domain := parsedURL.Hostname()
+
+		// if domain is not in map, add it
+		if _, exits := domainStatusMap[domain]; !exits {
+			domainStatusMap[domain] = &DomainStatus{}
+		}
+		domainStatusMap[domain].Requests++
+
+		// create a new request
+		method := endpoint.Method
+		if method == "" {
+			method = "GET"
+		}
+
+		var bodyReader *strings.Reader
+		if endpoint.Body != "" {
+			bodyReader = strings.NewReader(endpoint.Body)
+		} else {
+			bodyReader = strings.NewReader("")
+		}
+
+		req, err := http.NewRequest(method, endpoint.URL, bodyReader)
+		if err != nil {
+			log.Printf("Error in request: %s", err)
+			continue
+		}
+
+		// append headers to request
+		for key, val := range endpoint.Headers {
+			req.Header.Set(key, val)
+		}
+
+		// make the request but get milliseconds res time
+		startTime := time.Now()
+		res, err := client.Do(req)
+		totalTime := time.Since(startTime).Milliseconds()
+
+		if err != nil {
+			// if error, its down
+			log.Printf("DOWN: %s, %s", domain, err)
+			continue
+		}
+
+		// don't forget to close body
+		res.Body.Close()
+
+		// check for 200 status code and proper latency < 500
+		if (res.StatusCode >= 200 && res.StatusCode < 300) && totalTime < 500 {
+			domainStatusMap[domain].UpCount++
+			log.Printf("UP: %s, %dms", domain, totalTime)
+		} else {
+			log.Printf("DOWN: %s, %dms", domain, totalTime)
+		}
+	}
 }
 
 func startTimer() {
